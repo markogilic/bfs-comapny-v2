8b896284769aca17d1a1dde2da14c91f0e1fa939
Marko <gilicmarko10@gmail.com>
2024-09-01 01:19:14 +0200

    verification email 02

--- app/auth/new-verification/page.tsx
+++ app/auth/new-verification/page.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import { Suspense } from 'react';
 import { NewVerificationForm } from '@/app/_components/auth/new-verification-form';
 
 const NewVerificationPage = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <NewVerificationForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <NewVerificationForm />
+      </Suspense>
     </div>
   );
 };
