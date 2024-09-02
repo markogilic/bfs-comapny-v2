895e25ead6beea1cefa90f5820a5d96fca0e22b3
Marko <gilicmarko10@gmail.com>
2024-09-02 23:18:30 +0200

    pass reset added

--- app/auth/new-password/page.tsx
+++ app/auth/new-password/page.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import BfsLogoBlue from '@/public/des/logo_bfs_blue.svg';
 import Image from 'next/image';
 import { NewPasswordForm } from '@/app/_components/auth/new-password-form';
+import { Suspense } from 'react';
 const NewPasswordPage = () => {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="w-[400px] py-12 px-12 rounded-lg border border-gray flex shadow-md flex-col gap-4 items-center justify-center">
         <Image src={BfsLogoBlue} alt="BFS Logo" width={84} />
-
-        <NewPasswordForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <NewPasswordForm />
+        </Suspense>
       </div>
     </div>
   );
