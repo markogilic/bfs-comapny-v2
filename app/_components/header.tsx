69d0222b07e99807b3980e6184ea3f093d81de62
Marko <gilicmarko10@gmail.com>
2024-09-13 23:13:21 +0200

    session test

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,12 +2,8 @@ import Image from 'next/image';
 import HeaderLogo from '../../public/des/BFS-logo.svg';
 import Link from 'next/link';
 import MainNavigation from './ui/MainNavigation';
-import { auth } from '@/auth';
 
-const Header = async () => {
-  const session = await auth();
-
-  console.log('Session from header', session);
+const Header = () => {
   return (
     <header className=" w-full fixed top-0 flex justify-between md:px-[150px] px-3 py-3 bg-slate-50 shadow-md bg-opacity-90 items-center z-[999] backdrop-blur-[0.5rem]">
       {/* logo div */}
